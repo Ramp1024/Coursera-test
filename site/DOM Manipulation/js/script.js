42b92aa905e3fc813e4224ca46605e662bd51bc0
Ramprakash <ramprakash1024@outlook.com>
2020-08-09 12:26:59 +0530

    DOM Manipulation

---
+++ site/DOM Manipulation/js/script.js
@@ -0,0 +1,21 @@
+//console.log(document.getElementById("title"));
+
+function sayHello() {
+	var name = 
+		document.getElementById("name").value;
+		var message="Hello " + name + "!";
+
+		document
+			.getElementById("content")
+			.textContent = message;
+
+		if(name==="student"){
+			var title =
+			 	document.querySelector("#title")
+			 	.textContent;
+			title += "&Lovin it!!"; 	
+				document.querySelector("#title")
+			 	.textContent=title;
+		}	
+
+}
